5df51f5097fa2d21103db45567ff814199e88bfa
Caio Moretti de Macedo <caiomoretti7@gmail.com>
2023-07-24 11:32:07 -0300

    changing versionins and encryption resources

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -4,26 +4,31 @@ resource "aws_s3_bucket" "terraform_state" {
   lifecycle {
     prevent_destroy = true
   }
+}
 
-  versioning {
-    enabled = true
+resource "aws_s3_bucket_versioning" "versioning_example" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration {
+    status = "Enabled"
   }
+}
 
-  server_side_encryption_configuration {
-    rule {
-        apply_server_side_encryption_by_default {
-            sse_algorithm = "AES256"
-        }
+resource "aws_s3_bucket_server_side_encryption_configuration" "example" {
+  bucket = aws_s3_bucket.terraform_state.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm     = "AES256"
     }
   }
 }
 
 resource "aws_dynamodb_table" "terraform_locks" {
-    name = "terraform-state-locking"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-        name = "LockID"
-        type = "S"
-    }
+  name = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
 }
